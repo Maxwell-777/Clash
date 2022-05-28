6c8af5d15dbe4bd474df4e98e3ce0e2f566cf057
yyn-777 <38640537+yyn-777@users.noreply.github.com>
2022-05-28 20:39:36 +0800

    Update surge-media.js

--- surge-media.js
+++ surge-media.js
@@ -51,7 +51,7 @@ const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (
       } else if (status==STATUS_AVAILABLE){
         //console.log(2)
         console.log(region)
-        disney_result="Disney*: 已经解锁 ➠ " +`${getFlagEmoji(region)} ` + region.toUpperCase()
+        disney_result="Disney: 已经解锁 ➠ " +`${getFlagEmoji(region)} ` + region.toUpperCase()
         // console.log(result["Disney"])
       } else if (status==STATUS_NOT_AVAILABLE) {
         //console.log(3)
